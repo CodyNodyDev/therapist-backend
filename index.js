b6a148afddddfbe15f4bbac6084bdfe06629dd59
ytvee <tyanbz@gmail.com>
2025-02-11 11:56:29 +0300

    updated code base

--- index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const userController = require('./controllers');
-
-router.get('/users', userController.getAllUsers);
-router.get('/users/:id', userController.getUser);
-
-
-module.exports = router;
